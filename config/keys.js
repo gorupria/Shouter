10e129fa900a1bb1f37ce44b9ba4db630b3092c8
Purushot <battle_g5@yahoo.com>
2018-09-04 00:00:11 +0300

    Added validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://purushot:purush0t@ds241012.mlab.com:41012/acidrain'
+  mongoURI: 'mongodb://purushot:purush0t@ds241012.mlab.com:41012/acidrain',
+  secretOrKey: 'secret'
 };
